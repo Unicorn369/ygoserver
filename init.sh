3eb218fea4312140fcdbc67b8a5cdda23e788afc
Unicorn369 <3038572935@qq.com>
2025-09-30 20:56:16 +0800

    [2025/09/30] srvpro

--- init.sh
+++ init.sh
@@ -41,7 +41,7 @@ fi
 for arg in "$@"; do
     case $arg in
         --version)
-            echo "YGOServer Build 2025/08/18"
+            echo "YGOServer Build 2025/09/30"
             shift && exit
             ;;
         --install-mono|mono)
