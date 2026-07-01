fc53c53784c314f8da8eed9422008a36e417a4a1
Unicorn369 <3038572935@qq.com>
2026-07-01 12:22:21 +0800

    init

---
+++ Dockerfile
@@ -0,0 +1,35 @@
+FROM node:lts-alpine3.20 AS builder
+ARG SRVPRO_PATH
+COPY ${SRVPRO_PATH} /srvpro 
+
+WORKDIR /srvpro
+RUN apk add --no-cache python3 make g++
+RUN npm ci
+RUN npm run build
+RUN npm install --prefix /opt/local -g pm2
+RUM mkdir app
+COPY dist app/
+COPY node_modules app/
+COPY package*.json app/
+COPY config.example.yaml app/config.yaml
+
+###############
+FROM node:lts-alpine3.20
+ARG TARGETARCH
+ARG SRVPRO_PATH
+ARG YGOPRO_PATH
+ARG INIT_FILE_PATH
+
+RUN apk add --no-cache ca-certificates
+COPY ${YGOPRO_PATH} /ygoserver 
+COPY --from=builder /srvpro/app/* /ygoserver/*
+COPY --from=builder /opt/local /usr/
+COPY ${INIT_FILE_PATH} /run.sh
+
+RUN chmod +x /ygoserver/ygopro/ygopro
+RUN chmod +x /run.sh
+
+WORKDIR /ygoserver
+
+ENTRYPOINT [ "/run.sh" ]
+#CMD [ "--version" ]
---
+++ init.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+SRVPRO_PATH=/ygoserver
+SRVPRO_SCRIPT=$SRVPRO_PATH/pm2-docker.json
+
+run_server() {
+cat > $SRVPRO_SCRIPT << 'INITEOF'
+{
+	"apps": [
+		{
+			"name": "ygoserver",
+			"script": "/ygoserver/dist/index.js",
+			"cwd": "/ygoserver"
+		}
+	]
+}
+INITEOF
+
+  pm2-docker start $SRVPRO_SCRIPT
+}
+
+# 检测文件是否存在
+rm -rf $SRVPRO_PATH/data
+mkdir -p $SRVPRO_PATH/config
+ln -sf $SRVPRO_PATH/config $SRVPRO_PATH/data
+if [ ! -f "$SRVPRO_PATH/config/config.yaml" ]; then
+    cp -rf $SRVPRO_PATH/config.yaml $SRVPRO_PATH/config/config.yaml
+fi
+if [ -f "$SRVPRO_PATH/config/config.yaml" ]; then
+    cp -rf $SRVPRO_PATH/config/config.yaml $SRVPRO_PATH/config.yaml
+fi
+
+for arg in "$@"; do
+    case $arg in
+        --version)
+            echo "SRVPro2 2026/07/01"
+            shift && exit
+            ;;
+        -*|--*)
+            echo "Illegal option $1"
+            ;;
+    esac
+    shift
+done
+
+#—————————— RUN ——————————
+run_server
+#—————————— END ——————————
